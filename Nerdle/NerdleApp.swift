2e760708692c1d37e3cec7a2e66fce24d6ede641
Daniel Taylor English <danieltaylorenglish@gmail.com>
2022-09-26 10:41:33 -0700

    Initial Commit

---
+++ Nerdle/NerdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NerdleApp.swift
+//  Nerdle
+//
+//  Created by Daniel Taylor English on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct NerdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
